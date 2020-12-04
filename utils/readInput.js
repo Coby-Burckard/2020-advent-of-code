f55b9a5048eb6656e8a5965cbc4afd3cd4b5a301
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-04 15:04:56 -0600

    utils update

--- utils/readInput.js
+++ utils/readInput.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
-const readInput = (file = 'input.txt') => {
+const readInput = (file = 'input.txt', splitOn = '\r') => {
   try {
-    const data = fs.readFileSync(file).toString().split('\r')
+    const data = fs.readFileSync(file).toString().split(splitOn)
     return data.map((value) => value.replace(/(\r\n|\n|\r)/gm, ""))
   }
   catch {
@@ -15,7 +15,19 @@ const readInputToNum = (file = 'input.txt') => {
   return data.map(value => value * 1)
 }
 
+const readInput4 = (file = 'input.txt', splitOn = '\r') => {
+  try {
+    const data = fs.readFileSync(file).toString().replace(/(\n)/gm, ' ')
+    const newData = data.split(splitOn)
+    return newData.map((value) => value.replace(/(\r\n|\n|\r)/gm, ""))
+  }
+  catch {
+    throw new Error(`unable to read from ${ file }`)
+  }
+}
+
 module.exports = {
   readInput: readInput,
+  readInput4: readInput4,
   readInputToNum: readInputToNum
 }
