88d2f14373e7b18d1b2243097cd23cc04b74b436
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-15 09:37:16 -0600

    day15

---
+++ day15/index.js
@@ -0,0 +1,37 @@
+const { readInput } = require('../utils/readInput')
+
+const partOne = (startValues) => {
+  const mem = {}
+  const history = []
+
+  startValues.forEach((value, index) => {
+    mem[value] = index
+    history.push(value)
+  })
+
+  let next = history.pop()
+  let last
+  mem[next] = undefined
+
+  for (let index = startValues.length - 1; index < 30000000; index++) {
+    const lastTimeSaid = mem[next]
+    last = next
+    // history.push(next)
+    mem[next] = index
+    next = lastTimeSaid === undefined ? 0 : index - lastTimeSaid
+
+    if (index % 1000 === 0) {
+      console.log(index)
+    }
+  }
+
+
+  return last
+}
+
+const main = () => {
+  const input = readInput()[0].split(',')
+  console.log(partOne(input))
+}
+
+main()
